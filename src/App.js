c5b903bcd334458fc102661fa7c8bbcaf4f64528
Samed Karakus <karakusnavy@gmail.com>
2019-12-26 20:39:05 +0300

    up

--- src/App.js
+++ src/App.js
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      data: [],
+      mapData: null
+    };
+
+  }
+
+
+
+  componentDidMount() {
+
+    var dbData = [{ product: 'Bardak' }, { product: 'Banyo' }, { product: 'Bakır' }, { product: 'Çatı' }, { product: 'Ev' }, { product: 'Elektrik' }, {
+      product: 'Dolap'
+    }, { product: 'Dalgıç' }, { product: 'Çanta' }]
+
+    this.setState({ data: dbData })
+
+  }
+
+  changeText(text) {
+    var Datas = this.state.data
+    var TextLength = text.length
+    var newMapData = []
+      for(var i = 0; i<Datas.length;i++){
+        if(this.state.data[i].product.substring(0,TextLength).toLowerCase() == text.toLowerCase())
+        {
+          
+          newMapData.push({product:this.state.data[i].product})
+          this.setState({mapData:newMapData})
+        }
+      }
+  }
+
+  render() {
+    return (
+      <div className="App" style={{marginTop:200}}>
+        <input type="text"  style={{padding:10,fontSize:20}} onChange={(text) => this.changeText(text.target.value)} />
+        <div style={{ justifyContent: 'center', alignItems: 'center' }} >
+
+          {
+            this.state.mapData != null ?
+              this.state.mapData.map((gelenveri =>
+                <li class="item" key={gelenveri.id} >
+                  <b style={{fontSize:20}} >{gelenveri.product}</b>
+
+                  <br />
+
+                </li>
+              ))
+              :
+              null
+          }
+        </div>
+      </div>
+    )
+  }
+}
